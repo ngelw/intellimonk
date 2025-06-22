e0e69ad3b4dc1707a0cc59b87aaa1c15e173f6ad
ngelw <angelshrestha2024@gmail.com>
2025-06-22 11:11:15 -0500

    Personal Book Library Works

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -3,4 +3,5 @@ from . views import *
 urlpatterns = [
     path('',HomePage.as_view()),
     path('upload',UploadBook.as_view()),
+    path('books',MyBooks.as_view()),
 ]
--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from . forms import BookForm
+from . models import Book
 # Create your views here.
 class HomePage(View):
     def get(self,request):
@@ -21,3 +22,8 @@ class UploadBook(View):
 
     def get(self, request):
         return render(request, "upload.html", {"form": BookForm()})
+    
+class MyBooks(View):
+    def get(self, request):
+        books = Book.objects.filter(addedby=request.user)
+        return render(request, "mybooks.html", {"books": books})
